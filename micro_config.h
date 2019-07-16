3b7cadb29302a74002df603daddbfbf4cd39eb59
samirMossad1 <samirmossad6@gmail.com>
2019-07-16 09:19:35 +0200

    added comments and proper micro includes

---
+++ micro_config.h
@@ -0,0 +1,41 @@
+
+/*
+ * 		Module: Microcontroller Configurations.
+ *
+ *		File Name: micro_config.h
+ *
+ * 	    Created on: July 16, 2019
+ *
+ *      Author: Samir Mossad Ibrahim
+ */
+
+
+#ifndef MICRO_CONFIG_H
+#define MICRO_CONFIG_H
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
