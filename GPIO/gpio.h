e395a289228fa95638fec769c8aada4a0a28e51d
samirMossad1 <samirmossad6@gmail.com>
2019-07-16 11:21:09 +0200

    added the needed enums and defined the members of the configuration structures

--- GPIO/gpio.h
+++ GPIO/gpio.h
@@ -20,35 +20,58 @@
 
 /*Needed Includes*/
 
-#include "../macros.h"
-#include "../micro_config.h"
-#include "../std_types.h"
+#include "macros.h"
+#include "micro_config.h"
+#include "std_types.h"
 
 
 
 
 /*This enum is used to declare Levels of GPIO*/
-typedef enum level
+typedef enum gpio_level
 {
 	_LOW,_HIGH
 
 }GPIO_LEVEL;
 
+typedef enum gpio_type
+{
+	_INPUT,_OUTPUT
+
+}GPIO_TYPE;
+
+typedef enum interrupt_edge
+{
+	_LOW_LEVEL,
+	_LOGICAL_CHANGE,
+	_FALLING_EDGE,
+	_RISING_EDGE
+
+}GPIO_INTERRUPT_EDGE;
+
 
 typedef struct GPIO_port
 {
+
+	GPIO_PORT PORT;
+	GPIO_TYPE PORT_TYPE;
 
 }GPIO_portConfigStruct;
 
 
 typedef struct GPIO_pin
 {
+	GPIO_PORT PORT;
+	GPIO_PIN  PIN;
+	GPIO_TYPE PIN_TYPE;
 
 }GPIO_pinConfigStruct;
 
 
 typedef struct GPIO_int
 {
+	GPIO_INTERRUPT_NUMBER INTERRUPT_NUMBER;
+	GPIO_INTERRUPT_EDGE   INTERRUPT_EDGE_SELECT;
 
 }GPIO_intConfigStruct;
 
@@ -62,8 +85,6 @@ void	GPIO_writePort(const GPIO_PORT, const uint8_t);
 
 void	GPIO_togglePort(const GPIO_PORT);
 
-
-
 bool	GPIO_pinInit(const GPIO_pinConfigStruct*);
 
 uint8_t	GPIO_readPin(const GPIO_PORT , const GPIO_PIN);
@@ -72,20 +93,13 @@ void GPIO_writePin(const GPIO_PORT , const GPIO_PIN ,const uint8_t);
 
 void GPIO_togglePin(const GPIO_PORT , const GPIO_PIN);
 
+void GPIO_pinPullUpInit(const GPIO_PORT , const GPIO_PIN );
 
-
-
+bool GPIO_intInitialize(const GPIO_intConfigStruct*);
 
 bool GPIO_intEnable(const GPIO_INTERRUPT_NUMBER);
 
 bool GPIO_intDisable(const GPIO_INTERRUPT_NUMBER);
-
-bool GPIO_intInitialize(const GPIO_intConfigStruct*);
-
-
-
-
-
 
 
 
