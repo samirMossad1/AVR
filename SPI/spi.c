3b8161b6882fb85f6fa1e57b0b665d594245c984
samirMossad1 <samirmossad6@gmail.com>
2019-07-24 02:02:32 +0200

    added the implementation of the ISR and the read status function

--- SPI/spi.c
+++ SPI/spi.c
@@ -15,8 +15,20 @@ static void (*SPI_callBackPtr)(void)=NULL_PTR;
 
 
 
+ISR(SPI_STC_vect)
+{
+	if(SPI_callBackPtr!=NULL_PTR)
+		SPI_callBackPtr();
+
+	SET_BIT(SPI_STATUS_REGISTER,SPI_INTERRUPT_FLAG);
+
+}
 
 
+uint8_t SPI_readStatus()
+{
+		return SPI_STATUS_REGISTER>>7;
+}
 
 
 bool SPI_disable()
