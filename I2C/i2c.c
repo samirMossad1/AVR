96fe352861005c8e97c8577cf27b430f38fa9af7
samirMossad1 <samirmossad6@gmail.com>
2019-07-29 19:36:37 +0200

    removed all the old members of the config struct and added one more static call back pointer for receiving

--- I2C/i2c.c
+++ I2C/i2c.c
@@ -6,8 +6,40 @@
 #define BIT_RATE(CPU_FR,SCL_FR) (((CPU_FR)/(2*SCL_FR))-8)
 
 
-static void (*TWI_CallBackPtr_g)=NULL_PTR;
+static void (*TWI_CallBackPtr_g[2])=
+{
+		NULL_PTR		/*Transmit CallBack Pointer*/
+		,NULL_PTR		/*Receive CallBack Pointer*/
+};
 
+
+
+
+
+ISR(TWI_vect)
+{
+
+	/*Transmit*/
+
+	if(TWI_CallBackPtr_g[0] != NULL_PTR)
+	{
+
+
+	}
+
+
+
+	/*Receive*/
+	if(TWI_CallBackPtr_g[1] != NULL_PTR)
+	{
+
+
+	}
+
+
+
+
+}
 
 
 
@@ -21,18 +53,9 @@ bool TWI_init(const TWI_ConfigStruct* TWI_structPtr)
 	/*clear flag*/
 	SET_BIT(TWI_CONTROL_REGISTER,TWI_INTERRUPT_FLAG);
 
-	TWI_CONTROL_REGISTER|=( (TWI_structPtr->TWI_INT) << TWI_INTERRUPT_ENABLE )|
-						  ( (TWI_structPtr->TWI_ACK) << TWI_ACKNOWLEDGE_ENABLE );
-
-
-
 	TWI_STATUS_REGISTER&=~(1<<TWI_PRESCALAR_0)&~(1<<TWI_PRESCALAR_1);
 
 	TWI_BIT_RATE_REGISTER=BIT_RATE(TWI_structPtr->CPU_Frequency,TWI_structPtr->SCL_Frequency);
-
-
-
-	TWI_CallBackPtr_g=TWI_structPtr->TWI_callBackPtr;
 
 	TWI_ADDRESSE_REGISTER = ( (TWI_structPtr->TWI_addresse)<<BIT_1);
 
@@ -46,8 +69,8 @@ void TWI_start()
 {
 
 	TWI_CONTROL_REGISTER|=(1<<TWI_INTERRUPT_FLAG)|
-						  (1<<TWI_START)|
-						  (1<<TWI_ENABLE);
+			(1<<TWI_START)|
+			(1<<TWI_ENABLE);
 
 	while(BIT_IS_CLEAR(TWI_CONTROL_REGISTER,TWI_INTERRUPT_FLAG));
 }
@@ -57,8 +80,8 @@ void TWI_stop()
 {
 
 	TWI_CONTROL_REGISTER|=(1<<TWI_INTERRUPT_FLAG)|
-						  (1<<TWI_STOP)|
-						  (1<<TWI_ENABLE);
+			(1<<TWI_STOP)|
+			(1<<TWI_ENABLE);
 
 	while(BIT_IS_CLEAR(TWI_CONTROL_REGISTER,TWI_INTERRUPT_FLAG));
 
@@ -70,17 +93,22 @@ void TWI_send(uint8_t data)
 	TWI_DATA_REGISTER=data;
 
 	TWI_CONTROL_REGISTER|=(1<<TWI_INTERRUPT_FLAG)|
-						  (1<<TWI_ENABLE);
+			(1<<TWI_ENABLE);
 
 	while(BIT_IS_CLEAR(TWI_CONTROL_REGISTER,TWI_INTERRUPT_FLAG));
 
 }
 
 
-uint8_t TWI_read()
+uint8_t TWI_read(const 	TWI_ACKNOWLEDGMENT TWI_ACK)
 {
+
+	RESET_BIT(TWI_CONTROL_REGISTER,TWI_ACKNOWLEDGE_ENABLE);
+
 	TWI_CONTROL_REGISTER|=(1<<TWI_INTERRUPT_FLAG)|
-						  (1<<TWI_ENABLE);
+			(1<<TWI_ENABLE)|
+			(TWI_ACK<<TWI_ACKNOWLEDGE_ENABLE);
+
 
 	while(BIT_IS_CLEAR(TWI_CONTROL_REGISTER,TWI_INTERRUPT_FLAG));
 
@@ -101,13 +129,13 @@ uint8_t TWI_getFlag(TWI_FLAG TWI_FLAG)
 	if(TWI_FLAG==_INTERRUPT_FLAG_TWI)
 	{
 
-	return MASK_BITS_THEN_SHIFT_LEFT(TWI_CONTROL_REGISTER,TWI_INTERRUPT_FLAG,BIT_7);
+		return MASK_BITS_THEN_SHIFT_LEFT(TWI_CONTROL_REGISTER,TWI_INTERRUPT_FLAG,BIT_7);
 
 	}
 	else
 	{
 
-	return MASK_BITS_THEN_SHIFT_LEFT(TWI_CONTROL_REGISTER,TWI_WRITE_COLLISION_FLAG,BIT_3);
+		return MASK_BITS_THEN_SHIFT_LEFT(TWI_CONTROL_REGISTER,TWI_WRITE_COLLISION_FLAG,BIT_3);
 
 	}
 
@@ -135,3 +163,10 @@ bool TWI_disable()
 
 	return TRUE;
 }
+
+
+
+
+/*
+ * INTERRUPTS !!!!
+ * */
