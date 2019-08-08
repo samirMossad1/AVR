5b49bb29ec8ffc52b17a0d29db269f7a867da23c
samirMossad1 <samirmossad6@gmail.com>
2019-08-08 11:43:00 +0200

    added the implementation of timer1 pwm init but not yet finalized

--- DCMOTOR/dc_motor.c
+++ DCMOTOR/dc_motor.c
@@ -5,9 +5,12 @@
 #include "gpio.h"
 
 
-#define GetCompareValue(x,y)  ( (x*TIMER0_TOP_VALUE)/100 )
+#define GetCompareValue(x,y)  ( (x*y)/100 )
 
 static bool timer0_pwmMode_init(void);
+static bool timer2_pwmMode_init(void);
+static bool timer1_pwmMode_init(uint8_t);
+
 
 
 bool DCMOTOR_init(const DCMOTOR_Structure *DCMOTOR_structPtr)
@@ -37,16 +40,19 @@ bool DCMOTOR_init(const DCMOTOR_Structure *DCMOTOR_structPtr)
 	case SPEED_OC_1_A:	enable.PORT=PORT_D;
 						enable.PIN=PIN_5;
 						enable.PIN_TYPE=_OUTPUT;
+						timer1_pwmMode_init((uint8_t)(_CHANNEL_A));
 						break;
 
 	case SPEED_OC_1_B:	enable.PORT=PORT_D;
 						enable.PIN=PIN_4;
 						enable.PIN_TYPE=_OUTPUT;
+						timer1_pwmMode_init((uint8_t)(_CHANNEL_B));
 						break;
 
 	case SPEED_OC_2:	enable.PORT=PORT_D;
 						enable.PIN=PIN_7;
 						enable.PIN_TYPE=_OUTPUT;
+						timer2_pwmMode_init();
 						break;
 
 	}
@@ -101,8 +107,12 @@ void DCMOTOR_changeSpeed(const DCMOTOR_Structure *DCMOTOR_structPtr,uint16_t spe
 	switch(DCMOTOR_structPtr->SPEED_PIN)
 	{
 
-	case SPEED_OC_0:   TIMER0_COMAPRE_REGISTER=GetCompareValue(speed,TIMER0_TOP_VALUE);
+	case SPEED_OC_0:   TIMER0_COMPARE_REGISTER=GetCompareValue(speed,TIMER0_TOP_VALUE);
+						break;
 
+
+	case SPEED_OC_2:   TIMER2_COMPARE_REGISTER=GetCompareValue(speed,TIMER2_TOP_VALUE);
+						break;
 	}
 
 
@@ -117,9 +127,61 @@ static bool timer0_pwmMode_init(void)
 
 	TIMER0_COUNT_REGISTER=TIMER0_CLEAR_MASK;
 
-	TIMER0_COMAPRE_REGISTER=_100_DUTY_CYCLE;
+	TIMER0_COMPARE_REGISTER=_100_DUTY_CYCLE;
 
 	TIMER0_CONTROL_REGISTER=TIMER_0_PWM_NON_INVERTING_OC0_WITH_F_CPU_MASK;
+
+	return TRUE;
+
+}
+
+static bool timer1_pwmMode_init(uint8_t timerChannel_t)
+{
+
+	//TIMER1B_CONTROL_REGISTER&=~(TIMER_1_DISABLE_MASK);
+
+	TIMER1A_CONTROL_REGISTER=0X00;
+	TIMER1B_CONTROL_REGISTER=0X00;
+	TIMER1_COUNT_REGISTER=0X00;
+
+	//TIMER1_COUNT_REGISTER=TIMER_1_CLEAR_MASK;
+
+//
+//	if(timerChannel_t == _CHANNEL_A)
+//	{
+		/*Channel A*/
+		TIMER1A_COMPARE_REGISTER=TIMER1_TOP_VALUE;
+		//TIMER1A_CONTROL_REGISTER&=~(TIMER_1_PWM_NON_INVERTING_OC1A_MASK);
+		TIMER1A_CONTROL_REGISTER=(TIMER_1_PWM_NON_INVERTING_OC1A_MASK);
+
+//	}
+//	else
+//	{
+//		/*Channel B*/
+//		TIMER1B_COMPARE_REGISTER=TIMER1_TOP_VALUE;
+//		TIMER1A_CONTROL_REGISTER&=~(TIMER_1_PWM_NON_INVERTING_OC1B_MASK);
+//		TIMER1A_CONTROL_REGISTER|=(TIMER_1_PWM_NON_INVERTING_OC1B_MASK);
+//
+//	}
+
+
+
+	TIMER1B_CONTROL_REGISTER=0X01;
+
+	return TRUE;
+
+}
+
+static bool timer2_pwmMode_init(void)
+{
+
+	TIMER2_CONTROL_REGISTER&=~(TIMER_2_DISABLE_MASK);
+
+	TIMER2_COUNT_REGISTER=TIMER2_CLEAR_MASK;
+
+	TIMER2_COMPARE_REGISTER=_100_DUTY_CYCLE;
+
+	TIMER2_CONTROL_REGISTER=TIMER_2_PWM_NON_INVERTING_OC2_WITH_F_CPU_MASK;
 
 	return TRUE;
 
